5d6591f6ccdbeb31f27d59285faa9169fbe68abb
ericzzy <ericzhuangzhaoyi@outlook.com>
2022-02-22 02:14:52 -0500

    add constants

---
+++ users/constants.py
@@ -0,0 +1,8 @@
+class constants:
+    """
+    This class contains CONSTANTS that will be used in users app to avoid magic numbers.
+    """
+    MAX_NAME_LENGTH = 100
+    MAX_PHONE_NUM_LENGTH = 50
+    MAX_EMAIL_LENGTH = 200
+    MAX_ROLE_LENGTH = 15
--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.utils.translation import gettext_lazy as _
 from django.db import models
+from .constants import constants
 
 
 # Create your models here.
@@ -7,6 +8,7 @@ class Person(models.Model):
     """
     This is the person model which represents each team member of the team.
     """
+
     # user roles
     class personRole(models.TextChoices):
         """
@@ -15,12 +17,12 @@ class Person(models.Model):
         ADMIN = "admin", _('Administrator')
         REGULAR = 'regular', _('Regular')
 
-    first_name = models.CharField(max_length=200)
-    last_name = models.CharField(max_length=200)
-    phone_number = models.CharField(max_length=50)
-    email = models.CharField(max_length=200)
+    first_name = models.CharField(max_length=constants.MAX_NAME_LENGTH)
+    last_name = models.CharField(max_length=constants.MAX_NAME_LENGTH)
+    phone_number = models.CharField(max_length=constants.MAX_PHONE_NUM_LENGTH)
+    email = models.CharField(max_length=constants.MAX_EMAIL_LENGTH)
     role = models.CharField(
-        max_length=15,
+        max_length=constants.MAX_ROLE_LENGTH,
         choices=personRole.choices,
         default=personRole.REGULAR,
     )
